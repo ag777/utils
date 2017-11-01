e250e39bac22535f5760c05652da45bc4fda167c
wanggz <ag777@vip.qq.com>
2017-11-01 15:01:24 +0800

    代码更新

---
+++ src/com/ag777/util/lang/thread/Counter.java
@@ -0,0 +1,48 @@
+package com.ag777.util.lang.thread;
+
+/**
+ * 线程安全计数器
+ * 
+ * @author ag777
+ * @version  create on 2017年11月1日,last modify at 2017年11月1日
+ */
+public class Counter {
+	private long i;
+	public Counter(int i) {
+		this.i = i;
+	}
+	
+	public Counter(long i) {
+		this.i = i;
+	}
+	
+	public long add(int b) {
+		synchronized (Counter.class) {
+			i = i+b;
+			return i;
+		}
+	}
+	
+	public long add() {
+		return add(1);
+	}
+	
+	public long subtract(int b) {
+		synchronized (Counter.class) {
+			i = i-b;
+			return i;
+		}
+	}
+	
+	public long subtract() {
+		return subtract(1);
+	}
+	
+	public long longValue() {
+		return i;
+	}
+	
+	public int intValue() {
+		return Long.valueOf(i).intValue();
+	}
+}
