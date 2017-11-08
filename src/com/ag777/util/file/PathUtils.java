9d5c90ff4ff348b509879821d4afef377678621e
wanggz <ag777@vip.qq.com>
2017-11-08 17:05:11 +0800

    代码更新
    PathUtils修改获取src路径的方法,jar包获取到的是同级目录;增加获取resource包下文件io流的方法

--- src/com/ag777/util/file/PathUtils.java
+++ src/com/ag777/util/file/PathUtils.java
@@ -1,15 +1,38 @@
 package com.ag777.util.file;
 
+import java.io.File;
+import java.io.InputStream;
+
+import com.ag777.util.lang.SystemUtils;
+
 /**
  * 路径工具类
  * 
  * @author ag777
- * @version 
+ * @version last modify at 2017年11月08日
  */
 public class PathUtils {
-
-	public static String getSrcPath() {
-		//System.out.println(GetPath.class.getClassLoader().getResource(""));  
-		return PathUtils.class.getResource("/").getPath().replaceAll("%20"," ");	//带空格的路径
+	
+	public static InputStream getAsStream(String filePath) {
+		return PathUtils.class.getClassLoader().getResourceAsStream(filePath);
+	}
+	
+	public static String srcPath() {
+		java.net.URL url = PathUtils.class.getProtectionDomain().getCodeSource().getLocation();
+		String filePath = url.getPath();
+		try {
+			filePath = java.net.URLDecoder.decode(filePath, "utf-8");
+		} catch (Exception e) {
+//			e.printStackTrace();
+		}
+		File file = new File(filePath);
+		if(!file.isDirectory()) {	//可能是jar包，因为结果是/xxxx/xx.jar,所以我们要去得到jar包的同级路径
+			return file.getParent();
+		}
+		return filePath;
+	}
+	public static void main(String[] args) {
+		System.out.println(srcPath());
+		System.out.println(new File("/e:/ccc/").isDirectory());
 	}
 }
