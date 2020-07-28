55ef37a17c151316d04fbfe628b60e09a65f4e98
ag777 <aaaggg777@qq.com>
2020-07-28 10:16:05 +0800

    [add]增加一个带抛出异常的BiConsumerE接口

--- src/main/java/com/ag777/util/lang/function/BiConsumerE.java
+++ src/main/java/com/ag777/util/lang/function/BiConsumerE.java
@@ -3,32 +3,32 @@ package com.ag777.util.lang.function;
 import java.util.Objects;
 
 /**
- * 带有抛出异常的Consumer类
+ * 带有抛出异常的BiConsumer类
  * @param <T> 方法参数
  */
 @FunctionalInterface
-public interface ConsumerE<T> {
-
+public interface BiConsumerE<T, U> {
     /**
-     * Performs this operation on the given argument.
+     * Performs this operation on the given arguments.
      *
-     * @param t the input argument
+     * @param t the first input argument
+     * @param u the second input argument
      */
-    void accept(T t) throws Throwable;
+    void accept(T t, U u) throws Throwable;
 
     /**
-     * Returns a composed {@code BiConsumer} that performs, in sequence, this
+     * Returns a composed {@code Consumer} that performs, in sequence, this
      * operation followed by the {@code after} operation. If performing either
      * operation throws an exception, it is relayed to the caller of the
      * composed operation.  If performing this operation throws an exception,
      * the {@code after} operation will not be performed.
      *
      * @param after the operation to perform after this operation
-     * @return a composed {@code BiConsumer} that performs in sequence this
+     * @return a composed {@code Consumer} that performs in sequence this
      * operation followed by the {@code after} operation
      */
-    default ConsumerE<T> andThen(ConsumerE<? super T> after) {
+    default BiConsumerE<T, U> andThen(BiConsumerE<? super T, ? super U> after) {
         Objects.requireNonNull(after);
-        return (T t) -> { accept(t); after.accept(t); };
+        return (T t, U u) -> { accept(t, u); after.accept(t, u); };
     }
 }
