4d47580974366edc25ef1e91579bd340db1955f7
ag777 <ag777@vip.qq.com>
2022-05-12 09:13:10 +0800

    [modify] 补充ThreadHelper

--- src/main/java/com/ag777/util/lang/thread/ThreadHelper.java
+++ src/main/java/com/ag777/util/lang/thread/ThreadHelper.java
@@ -1,5 +1,7 @@
 package com.ag777.util.lang.thread;
 
+import com.ag777.util.lang.interf.Disposable;
+
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -7,9 +9,9 @@ import java.util.concurrent.*;
  * 多线程工具类(只辅助,外部靠内存共享数据实现结果处理)
  * 
  * @author ag777
- * Time: created at 2017/3/21. last modify at 2022/05/11.
+ * Time: created at 2017/3/21. last modify at 2022/05/12.
  */
-public class ThreadHelper<T> {
+public class ThreadHelper<T> implements Disposable {
 
 	private ThreadGroup threadGroup;
 	private List<Thread> threadList;
@@ -25,6 +27,12 @@ public class ThreadHelper<T> {
 
 	/*==============添加单个任务==============*/
 
+	/**
+	 * 添加任务
+	 * @param token 用于获取结果的钥匙
+	 * @param callable 任务实现
+	 * @return ThreadHelper
+	 */
 	public ThreadHelper<T> addTask(String token, Callable<T> callable) {
 		FutureTask<T> task = new FutureTask<>(callable);
 		result.put(token, task);
@@ -34,7 +42,11 @@ public class ThreadHelper<T> {
 		return this;
 	}
 
-
+	/**
+	 *
+	 * @return {token: FutureTask}
+	 * @throws InterruptedException 线程中断
+	 */
 	public Map<String, FutureTask<T>> getResult() throws InterruptedException {
 		if (!isFinish) {
 			join();
@@ -42,10 +54,36 @@ public class ThreadHelper<T> {
 		return result;
 	}
 
+	/**
+	 *
+	 * @param token 钥匙
+	 * @return 任务执行结果
+	 * @throws IllegalArgumentException 任务不存在
+	 * @throws InterruptedException 线程中断
+	 * @throws ExecutionException 任务执行异常
+	 */
+	public T getResult(String token) throws IllegalArgumentException, InterruptedException, ExecutionException {
+		if (!isFinish) {
+			join();
+		}
+		FutureTask<T> task = result.get(token);
+		if (task == null) {
+			throw new IllegalArgumentException("不存在的token");
+		}
+		return task.get();
+	}
 
-	private ThreadHelper<T> join() throws InterruptedException {
-		for (Thread t : threadList) {
-			t.join();
+
+	/**
+	 * 等待所有线程执行完成
+	 * @return ThreadHelper
+	 * @throws InterruptedException 线程中断
+	 */
+	private synchronized ThreadHelper<T> join() throws InterruptedException {
+		Iterator<Thread> itor = threadList.iterator();
+		while (itor.hasNext()) {
+			itor.next().join();
+			itor.remove();
 		}
 		isFinish = true;
 		return this;
@@ -75,9 +113,24 @@ public class ThreadHelper<T> {
 			return "返回数据1";
 		});
 		t.addTask("t2", () ->  "返回数据2");
+		t.addTask("t3", ()-> {
+			throw new Exception("aaa");
+		});
+		System.out.println(t.getResult("t2"));
 		Map<String, FutureTask<String>> result = t.getResult();
 		for (String token : result.keySet()) {
 			System.out.println(token+": "+result.get(token).get());
 		}
+	}
+
+	@Override
+	public void dispose() {
+		if (threadGroup != null && !isFinish) {
+			threadGroup.interrupt();
+			threadGroup = null;
+			threadList = null;
+			result = null;
+		}
+
 	}
 }
